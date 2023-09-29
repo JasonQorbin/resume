f2f4823432b8b06685a4fd8dd08146dc622edb28
Jason Pieterse <Jason.Pieterse@Outlook.com>
2023-09-29 18:39:48 +0200

    Implement progress bars.

--- js/main.js
+++ js/main.js
@@ -5,4 +5,29 @@ $(function() {
     });
 });
 
-console.log("testing");
+function createProgressBarElements(progressBarElement, progress){
+	for (let i = 0; i < 10; ++i) {
+		let barSegment = document.createElement("span");
+		barSegment.classList.add("progress-bar-segment");
+		let progressClass = (progress-1) >= i ? "segment-state-on" : "segment-state-off";
+		barSegment.classList.add(progressClass);
+		if (i == 0) {
+			barSegment.classList.add("first-segment");
+		}
+		
+		if (i == 9) {
+			barSegment.classList.add("last-segment");
+		}
+		progressBarElement.appendChild(barSegment);
+	}
+}
+
+function insertProgressBars() {
+	let barContainers = document.querySelectorAll(".progress-bar");
+	barContainers.forEach((container) => {
+		const progress = parseInt(container.getAttribute("value"));
+		createProgressBarElements(container, progress);
+	})
+}
+
+insertProgressBars();
